bfa336ec3a0de10bfcde368e0000dadec84b599b
rodridega <56453875+rodridega@users.noreply.github.com>
2022-09-04 19:45:59 -0300

    Rutas creadas funcionando

--- index.js
+++ index.js
@@ -1,65 +1,26 @@
 const express = require("express");
-const { Server: HttpServer } = require("http");
-const { Server: IOServer } = require("socket.io");
+require('dotenv').config()
+
 
 const app = express();
-const { router, productos } = require("./src/router/routes");
+const { routerProductos, routerCarrito } = require("./src/router/routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// SOCKET
-const httpServer = new HttpServer(app);
-const io = new IOServer(httpServer);
-
-const messages = [];
-
-//HANDLEBARS
-const { engine } = require("express-handlebars");
-const readChat = require("./src/utils/readChat");
-const saveProduct = require("./src/utils/saveProduct");
-const insertChat = require("./src/utils/insertChat");
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./src/Handlebars/views");
-
-
 app.get("/", (req, res) => {
-  res.render("datos");
-});
-
-app.use("/api/productos", router);
-
-io.on("connection", async (socket) => {
-  console.log("a user connected");
-
-  socket.emit("todosLosProductos", productos);
-
- 
- const chatINFO = await readChat();
-  
-
-socket.emit("todosLosMensajes", chatINFO);
-   
-
-socket.on("productoGuardado", async (data) => {
-    await saveProduct(data);
-    io.sockets.emit("todosLosProductos", productos);
-  });
- 
-
-  socket.on("nuevoMensaje", async (data) => {
-    await insertChat(data);
-    io.sockets.emit("todosLosMensajes", await readChat());
-  });
-});
-
-const PORT = process.env.PORT || 8080;
-
-const connectedServer = httpServer.listen(PORT, () => {
-  console.log("Servidor http con web sockets listo")
+  res.send("/index.html")
 })
 
-connectedServer.on("error", error => console.log)
+app.use("/api/productos", routerProductos);
 
+app.use("/api/carrito", routerCarrito)
+
+
+const PORT = process.env.PORT|| 8080 ;
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT} `);
+})
+ 
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,74 +1,189 @@
 const { Router } = require("express");
 
-const router = Router();
+const routerProductos = Router();
+const routerCarrito = Router();
 
-const productos = [];
+let productos = [];
+let carritos = [];
 
 let prodID = 1;
+let carritoId = 1;
 
-router.get('/', (req, res) => {
-    // GET '/api/productos' -> devuelve todos los productos.
-    res.send(productos)
-})
+//Admin
+const admin = false;
 
-router.get('/:id', (req, res) => {
-    // GET '/api/productos/:id' -> devuelve un producto según su id.
-    const { id } = req.params
-    const prod = productos.filter((prod) => prod.id == id)
-    if (prod.length == 0) res.status(400).json({ error: 'producto no encontrado' })
-    res.send(prod)
-})
+routerProductos.get("/", (req, res) => {
+  res.json(productos);
+});
 
-router.post('/', (req, res) => {
-    // POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
-    const { nombre, precio, urlImagen } = req.body
-    if (!nombre || !precio || !urlImagen) {
-        res.status(400).json({ error: 'por favor ingrese todos los datos' })
-    } else {
-        const data = { nombre, precio, urlImagen }
-        data.id = prodID
-        productos.push(data)
-        prodID += 1
-        res.send(data)
-    }
-})
+routerProductos.get("/:id", (req, res) => {
+  // GET '/api/productos/:id' -> devuelve un producto según su id.
+  const { id } = req.params;
+  const prod = productos.filter((prod) => prod.id == id);
+  if (prod.length == 0)
+    res.status(400).json({ error: "producto no encontrado" });
+  res.send(prod);
+});
 
-router.put('/:id', (req, res) => {
-    // PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
-    const { id } = req.params
-    const { nombre, precio, urlImagen } = req.body
-    if (!nombre || !precio || !urlImagen) {
-        res.status(400).json({ error: 'producto no encontrado' })
-    } else {
-        let contador = 0
-        for (let i = 0; i < productos.length; i++) {
-            if (productos[i].id == id) {
-                productos[i].nombre = nombre
-                productos[i].precio = precio
-                productos[i].urlImagen = urlImagen
-                contador += 1
-                break
-            }
-        }
-        if (contador == 0) res.status(400).json({ error: 'producto no encontrado' })
-        res.send(productos)
-    }
-})
+routerProductos.post("/", (req, res) => {
+  if (!admin) {
+    return res
+      .status(401)
+      .send({ error: -1, descripcion: "ruta / metodo POST no autorizada" });
+  }
+  // POST '/api/productos' -> recibe y agrega un producto, y lo devuelve con su id asignado.
+  const { nombre, precio, urlImagen, descripcion, codigo, stock } = req.body;
+  if (!nombre || !precio || !urlImagen || !descripcion || !codigo || !stock) {
+    res.status(400).json({ error: "por favor ingrese todos los datos" });
+  } else {
+    const data = {
+      nombre,
+      precio,
+      urlImagen,
+      fechaCreacion: Date.now(),
+      descripcion,
+      id: prodID,
+      codigo,
+      stock,
+    };
 
-router.delete('/:id', (req, res) => {
-    // DELETE '/api/productos/:id' -> elimina un producto según su id.
-    const { id } = req.params
-    const largoActual = productos.length
+    productos.push(data);
+    prodID += 1;
+    res.send(data);
+  }
+});
+
+routerProductos.put("/:id", (req, res) => {
+  if (!admin) {
+    return res
+      .status(401)
+      .send({ error: -1, descripcion: "ruta /:id metodo PUT no autorizada" });
+  }
+  // PUT '/api/productos/:id' -> recibe y actualiza un producto según su id.
+  const { id } = req.params;
+  const { nombre, precio, urlImagen, descripcion, codigo, stock } = req.body;
+  if (!nombre || !precio || !urlImagen) {
+    res.status(400).json({ error: "producto no encontrado" });
+  } else {
+    let contador = 0;
     for (let i = 0; i < productos.length; i++) {
-        if (productos[i].id == id) {
-            productos.splice(i, 1)
-        }
+      if (productos[i].id == id) {
+        productos[i].nombre = nombre;
+        productos[i].precio = precio;
+        productos[i].urlImagen = urlImagen;
+        productos[i].descripcion = descripcion;
+        productos[i].codigo = codigo;
+        productos[i].stock = stock;
+
+        contador += 1;
+        break;
+      }
     }
-    if (productos.length == largoActual) res.status(400).json({ error: 'producto no encontrado' })
-    res.send(productos)
-})
+    if (contador == 0)
+      res.status(400).json({ error: "producto no encontrado" });
+    res.json(productos);
+  }
+});
+
+routerProductos.delete("/:id", (req, res) => {
+  if (!admin) {
+    return res
+      .status(401)
+      .send({ error: -1, descripcion: "ruta /:id metodo DELETE no autorizada" });
+  }
+  // DELETE '/api/productos/:id' -> elimina un producto según su id.
+  const { id } = req.params;
+  const largoActual = productos.length;
+  for (let i = 0; i < productos.length; i++) {
+    if (productos[i].id == id) {
+      productos.splice(i, 1);
+    }
+  }
+  if (productos.length == largoActual)
+    res.status(400).json({ error: "producto no encontrado" });
+  res.send(productos);
+});
+
+routerCarrito.post("/", (req, res) => {
+  const productosCarrito = productos;
+
+  const carrito = {
+    id: carritoId,
+    fechaCreacion: Date.now(),
+    productos: productosCarrito,
+  };
+  carritos.push(carrito);
+  productos = [];
+  prodID = 1;
+  carritoId += 1;
+
+  res.json(carrito.id);
+});
+
+routerCarrito.get("/", (req, res) => {
+  res.json(carritos);
+});
+
+routerCarrito.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const largoActual = carritos.length;
+  for (let i = 0; i < carritos.length; i++) {
+    if (carritos[i].id == id) {
+      carritos.splice(i, 1);
+    }
+  }
+  if (carritos.length == largoActual)
+    res.status(400).json({ error: "producto no encontrado" });
+  res.send(carritos);
+});
+
+routerCarrito.get("/:id/productos", (req, res) => {
+  const { id } = req.params;
+
+  res.json(carritos[id - 1].productos);
+});
+
+routerCarrito.post("/:id/productos", (req, res) => {
+  const { id } = req.params;
+  const { nombre, precio, urlImagen, descripcion, codigo, stock } = req.body;
+  if (!nombre || !precio || !urlImagen || !descripcion || !codigo || !stock) {
+    res.status(400).json({ error: "por favor ingrese todos los datos" });
+  } else {
+    const newProduct = {
+      nombre,
+      precio,
+      urlImagen,
+      fechaCreacion: Date.now(),
+      descripcion,
+      id: prodID,
+      codigo,
+      stock,
+    };
+    carritos[id - 1].productos.push(newProduct);
+
+    res.json(carritos[id].productos);
+  }
+});
+
+routerCarrito.delete("/:id/productos/:id_prod", (req, res) => {
+  const { id, id_prod } = req.params;
+
+  carritos = carritos.map((carrito) => {
+    if (carrito.id == id) {
+      const carritoFiltered = {
+        ...carrito,
+        productos: carrito.productos.filter((prod) => prod.id != id_prod),
+      };
+      return carritoFiltered;
+    }
+    return carrito;
+  });
+
+  res.json(carritos);
+});
 
 module.exports = {
-  router: router,
-  productos: productos,
+  routerProductos,
+  routerCarrito,
+  productos,
 };
